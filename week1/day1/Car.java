084888bb0021c82fd029452e4e42a62ddb7db386
suryaanandasundaram <114812349+suryaanandasundaram@users.noreply.github.com>
2022-10-08 20:30:23 +0530

    Add files via upload
    Week1-Day1

---
+++ week1/day1/Car.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Car 
+
+{	
+	byte number1=8;
+	
+	short number2= -327;
+	int number3 =985883920;
+	
+	
+	float number4 =689.93682F;
+	double number5 =790283.7410;
+	
+	char inital ='c';
+	String name="Suryaprabha";
+}
+
