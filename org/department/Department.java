68c820978da6d247b68a50d60d43d26defec806a
suryaanandasundaram <114812349+suryaanandasundaram@users.noreply.github.com>
2022-10-15 17:02:37 +0530

    Delete org directory

--- org/department/Department.java
+++
@@ -1,9 +0,0 @@
-package org.department;
-
-import org.college.College;
-
-public class Department extends College {
-	public void deptName() {
-		System.out.println("DepartName - CSE");
-	}
-}
--- org/system/Computer.java
+++
@@ -1,8 +0,0 @@
-package org.system;
-
-public class Computer {
-	protected void computerModel() {
-		System.out.println("The System Model -HP Laptop 15s-gr0xxx");
-	}
-
-}
