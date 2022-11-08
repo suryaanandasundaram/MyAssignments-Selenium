13dbfa5c3f091ebe55bce3854b69ee7ba4ccfe67
suryaanandasundaram <114812349+suryaanandasundaram@users.noreply.github.com>
2022-11-08 14:00:11 +0530

    Add files via upload

---
+++ week6/day2/utils/ReadExcelServiceNow.java
@@ -0,0 +1,38 @@
+package utils;
+
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcelServiceNow {
+public static String[][] readData(String fileName) throws IOException {
+	
+	XSSFWorkbook book = new XSSFWorkbook("./testData-ServiceNow/"+fileName+".xlsx");
+	XSSFSheet sheet = book.getSheetAt(0);
+	
+	int rowCount = sheet.getLastRowNum();
+	short colCount = sheet.getRow(0).getLastCellNum();
+	
+	String[][] data = new String[rowCount][colCount];
+	
+	for(int i=1;i<=rowCount;i++)
+	{
+		XSSFRow eachrow = sheet.getRow(i);
+		
+		for(int j=0;j<colCount;j++)
+		{
+			XSSFCell eachcell = eachrow.getCell(j);
+			String stringCellValue = eachcell.getStringCellValue();
+			data[i-1][j]=stringCellValue;
+			
+		}
+	}
+	
+	book.close();
+	return data;
+	
+}		
+}
