09fc9f2b982a77eda235f2a761a7c24fa74d48ca
suryaanandasundaram <114812349+suryaanandasundaram@users.noreply.github.com>
2022-11-08 14:02:37 +0530

    Add files via upload

---
+++ week6/day2/dataprovider/leaftaps/DPDeleteLeadLT.java
@@ -0,0 +1,94 @@
+package dataprovider.leaftaps;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class DPDeleteLeadLT extends DPBaseClassLT{
+	
+	@BeforeTest
+	public String setfileName()
+	{
+		fileName="TC005-DPDeleteLead";
+		return fileName;
+	}
+	
+	
+	
+	@Test(dataProvider="testDataLT")
+	public  void runDPDeleteLeadLT(String uname,String pwd) 
+	{
+		
+		
+		WebElement elementUserName = driver.findElement(By.id("username"));
+		elementUserName.sendKeys(uname);
+		
+		WebElement elementPassWord = driver.findElement(By.id("password"));
+		elementPassWord.sendKeys(pwd);
+		
+		WebElement elementLogin = driver.findElement(By.className("decorativeSubmit"));
+		elementLogin.click();
+		
+		WebElement elementLink = driver.findElement(By.linkText("CRM/SFA"));
+		elementLink.click();
+		
+		WebElement elementLeads = driver.findElement(By.linkText("Leads"));
+		elementLeads.click();
+		
+		WebElement elementFindLead = driver.findElement(By.linkText("Find Leads"));
+		elementFindLead.click();
+		
+	
+		WebElement elementFirst = driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId'][1]"));
+		String text = elementFirst.getText();
+		System.out.println(text);
+		
+		WebElement elementFirst1=driver.findElement(By.linkText(text));
+		
+		elementFirst1.click();
+		
+	
+		
+		System.out.println("The First Resulting lead ID " +text);
+		
+		
+		WebElement elementDelete = driver.findElement(By.linkText("Delete"));
+		elementDelete.click();
+		
+		WebElement elementFindLead1 = driver.findElement(By.linkText("Find Leads"));
+		elementFindLead1.click();
+		
+		WebElement elementFindLeadId = driver.findElement(By.name("id"));
+		elementFindLeadId.sendKeys(text);
+		
+		WebElement elementFindLeads = driver.findElement(By.xpath("//table[@id='ext-gen242']"));
+		elementFindLeads.click();
+		
+		
+		
+		
+	WebElement elementDisplay = driver.findElement(By.xpath("//div[text()='No records to display']"));
+	
+	//WebElement elementDisplay = driver.findElement(By.linkText("No records to display"));
+	String string1 = elementDisplay.getText();
+	
+		
+	
+	System.out.println(string1);
+	
+		if(string1.contains("No records to display"))
+		{
+			System.out.println("This message confirms the successful deletion");
+		}
+		
+		else
+		{
+			System.out.println("The record is not deleted ");
+		}
+	
+}
+}
